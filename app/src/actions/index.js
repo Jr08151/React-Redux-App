c5ce674d8183f85195ab8aac28811275f4719e5d
Jonathan Richardson <jr@Jonathans-Mini.tampabay.rr.com>
2020-08-27 19:41:56 -0400

    updated

--- app/src/actions/index.js
+++ app/src/actions/index.js
@@ -7,10 +7,10 @@ export const FETCH_FAILURE = 'FETCH_FAILURE'
 export const fetchData = () => dispatch => {
     dispatch({ type: START_FETCHING })
     axios
-    .get("https://api.sportsdata.io/v3/mlb/scores/json/AllTeams", {
+    .get("https://api.sportsdata.io/v3/mlb/scores/json/teams", {
         "method": "GET",
         "headers": 	{	
-            "Ocp-Apim-Subscription-Key": "8b6b2979daf04317a5c067a795998b5f"
+            "Ocp-Apim-Subscription-Key": "65ec793f0ea8437a866b80c0aca7fd99"
             }
         })
     .then (res => {
